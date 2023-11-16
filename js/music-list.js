c0e66867a373df2c4cff2b5292c72ee1510f2f99
nguyenduong <duonghp1991@gmail.com>
2023-11-16 23:49:47 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,33 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ai mang cô đơn đi",
+    artist: "K-ICM",
+    img: "Ai mang cô đơn đi",
+    src: "music-1"
+  },
+  {
+    name: "Anh Thương Em Mà",
+    artist: "Anh Rồng",
+    img: "Anh Thương Em Mà",
+    src: "music-2"
+  },
+  {
+    name: "Anh Ơi Ở Lại",
+    artist: "Anh Rồng",
+    img: "Anh Ơi Ở Lại",
+    src: "music-3"
+  },
+];
+
+//
